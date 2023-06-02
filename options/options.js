8a0b968f410cbf8dcff1e574ccf0a11c332c1cb9
Crowdbotics <team@crowdbotics.com>
2023-06-02 22:14:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quick-buy-41205",
+  url: "https://quick-buy-41205.botics.co",
+  api: "https://quick-buy-41205.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
